d3032a4b76f29f8e07c21ceb9c974a923c002335
Ewu52 <ethan.wu25@ycdsbk12.ca>
2023-10-12 19:04:20 -0400

    entered the code

--- Sketch.java
+++ Sketch.java
@@ -22,15 +22,35 @@ public class Sketch extends PApplet {
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  public void draw() {  
+    // set variables
+    float flowerX = random(0, width);
+    float flowerY = random(0, height);
+    float flowerDiameter = random(10, 200);
+    float stemX = random(0, 200);
+    float stemWidth = random(10, 80);
+    float stemHeight = random(10, 250);
+    float skyX = width;
+    float skyY = 250;
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+	  /** Use large rectangle to create the sky, make the rectangle blue */ 
+    fill(105, 245, 231);
+    rect(0, 0, skyX, skyY);
+
+    /** Create thin vertical rectangle as the flower stem, make it dark green */    
+    fill(0, 115, 25);
+    rect(stemX, flowerY, stemWidth, stemHeight);
   
-  // define other methods down here.
+    /** Create circles for the flower petals */  
+    fill(250, 238, 2);
+    ellipse(flowerX - 40, flowerY - 40, flowerDiameter + 10, flowerDiameter + 10);
+    ellipse(flowerX + 40, flowerY - 40, flowerDiameter + 10, flowerDiameter + 10);
+    ellipse(flowerX - 40, flowerY + 40, flowerDiameter + 10, flowerDiameter + 10);
+    ellipse(flowerX + 40, flowerY + 40, flowerDiameter + 10, flowerDiameter + 10);
+
+    /** Create circle to represent the center of the flower */
+    fill(20, 23, 20);
+    ellipse(flowerX, flowerY, flowerDiameter, flowerDiameter);
+
+  }
 }
